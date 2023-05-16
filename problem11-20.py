4950d1b99df3d98fe686b0d04a1d18a69be03519
Dawid Nowak <dawid.nowak040@gmail.com>
2023-05-16 12:50:22 +0200

    problem 12 solved

--- problem11-20.py
+++ problem11-20.py
@@ -1,15 +1,15 @@
 def problem12():
+    # Highly divisible triangular number
+
     divisors = []
     number = 0
     i = 0
     while len(divisors) < 501:
-        i = i + 1
+        i += 1
         number += i
         divisors = []
-        for n in range(number+1):
+        for n in range(int(number/2)+1):
             if number % (n+1) == 0:
                 divisors.append(n+1)
+        divisors.append(number)
         print(number)
-
-
-problem12()
